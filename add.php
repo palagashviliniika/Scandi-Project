f12f8869d584f490dc4a5579b872f02fa527f862
palagashviliniika <n_palagashvili@cu.edu.ge>
2022-03-25 03:24:29 +0400

    fixed add navbar

--- add.php
+++ add.php
@@ -9,13 +9,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Styles/main.css">
     <title>Document</title>
+    <script
+            src="https://code.jquery.com/jquery-3.6.0.js"
+            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+            crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/c55fd663d8.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-
-<div class="page">
-
-    <?php include "templates/header.php" ?>
+    <div class="nav">
+        <div class="container">
+            <div class="title">
+                <a class="fa fa-solid fa-store" href="index.php"></a>
+                <a class="name" href="index.php">Task Page</a>
+            </div>
+        </div>
+    </div>
 
     <div class="add">
 
@@ -67,8 +79,6 @@
     </div>
 
     <?php include "templates/footer.php";?>
-
-</div>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -16,110 +16,103 @@ $furnitures = $furnitureProducts -> showTypes('Furniture', 'FurnitureProducts');
 
 <!doctype html>
 <html lang="en">
-
-<div class="page">
     
-    <?php include 'templates/header.php'; ?>
+<?php include 'templates/header.php'; ?>
 
+<div class="products">
+    <div class="container">
 
+        <h1>Browse Products!</h1>
 
+        <form action="requests/delete_request.php" method="post" id="delete_product_form">
 
-    <div class="products">
-        <div class="container">
+            <h2>Netflix? Who Needs Them?!</h2>
 
-            <h1>Browse Products!</h1>
+            <div class="section">
 
-            <form action="requests/delete_request.php" method="post" id="delete_product_form">
+                <?php foreach ($dvds as $dvd){ ?>
 
-                <h2>Netflix? Who Needs Them?!</h2>
+                    <div class="card">
 
-                <div class="section">
-
-                    <?php foreach ($dvds as $dvd){ ?>
-
-                        <div class="card">
-
-                            <div class="checkbox">
-                                <label for="">
-                                    <input type="checkbox" class="delete-checkbox" name="delete-checkbox[]" value="<?php echo htmlspecialchars($dvd['id']); ?>">
-                                    Choose to delete
-                                </label>
-                            </div>
-
-                            <div class="card_content">
-                                <h5><?php echo htmlspecialchars($dvd['sku']); ?></h5>
-                                <h3><?php echo htmlspecialchars($dvd['name']); ?></h3>
-                                <h5><?php echo htmlspecialchars($dvd['price']).'$'; ?></h5>
-                                <h5><?php echo 'Size: '.htmlspecialchars($dvd['size']).'MB'; ?></h5>
-                            </div>
-                        </div>
-                    
-
-                    <?php } ?>
-
-                </div>
-
-                <h2>a Book a Day Keeps Reality Away!</h2>
-
-                <div class="section">
-
-                    <?php foreach ($books as $book){ ?>
-
-                        <div class="card">
-
-                            <div class="checkbox">
-                                <label for="">
-                                    <input type="checkbox" class="delete-checkbox" name="delete-checkbox[]" value="<?php echo htmlspecialchars($book['id']); ?>">
-                                    Choose to delete
-                                </label>
-                            </div>
-
-                            <div class="card_content">
-                                <h5><?php echo htmlspecialchars($book['sku']); ?></h5>
-                                <h3><?php echo htmlspecialchars($book['name']); ?></h3>
-                                <h5><?php echo htmlspecialchars($book['price']).'$'; ?></h5>
-                                <h5><?php echo 'Weight: '.htmlspecialchars($book['weight']).'KG'; ?></h5>
-                            </div>
+                        <div class="checkbox">
+                            <label for="">
+                                <input type="checkbox" class="delete-checkbox" name="delete-checkbox[]" value="<?php echo htmlspecialchars($dvd['id']); ?>">
+                                Choose to delete
+                            </label>
                         </div>
 
-                    <?php } ?>
+                        <div class="card_content">
+                            <h5><?php echo htmlspecialchars($dvd['sku']); ?></h5>
+                            <h3><?php echo htmlspecialchars($dvd['name']); ?></h3>
+                            <h5><?php echo htmlspecialchars($dvd['price']).'$'; ?></h5>
+                            <h5><?php echo 'Size: '.htmlspecialchars($dvd['size']).'MB'; ?></h5>
+                        </div>
+                    </div>
+                
 
-                </div>
+                <?php } ?>
 
-                <h2>Comfy, Right?</h2>
+            </div>
 
-                <div class="section">
+            <h2>a Book a Day Keeps Reality Away!</h2>
 
-                    <?php foreach ($furnitures as $furniture){ ?>
+            <div class="section">
 
-                        <div class="card">
+                <?php foreach ($books as $book){ ?>
 
-                            <div class="checkbox">
-                                <label for="">
-                                    <input type="checkbox" class="delete-checkbox" name="delete-checkbox[]" value="<?php echo htmlspecialchars($furniture['id']); ?>">
-                                    Choose to delete
-                                </label>
-                            </div>
+                    <div class="card">
 
-                            <div class="card_content">
-                                <h5><?php echo htmlspecialchars($furniture['sku']); ?></h5>
-                                <h3><?php echo htmlspecialchars($furniture['name']); ?></h3>
-                                <h5><?php echo htmlspecialchars($furniture['price']).'$'; ?></h5>
-                                <h5><?php echo 'Dimensions: '.htmlspecialchars($furniture['height']).'x'.htmlspecialchars($furniture['width']).'x'.htmlspecialchars($furniture['length']); ?></h5>
-                            </div>
+                        <div class="checkbox">
+                            <label for="">
+                                <input type="checkbox" class="delete-checkbox" name="delete-checkbox[]" value="<?php echo htmlspecialchars($book['id']); ?>">
+                                Choose to delete
+                            </label>
                         </div>
 
-                    <?php } ?>
+                        <div class="card_content">
+                            <h5><?php echo htmlspecialchars($book['sku']); ?></h5>
+                            <h3><?php echo htmlspecialchars($book['name']); ?></h3>
+                            <h5><?php echo htmlspecialchars($book['price']).'$'; ?></h5>
+                            <h5><?php echo 'Weight: '.htmlspecialchars($book['weight']).'KG'; ?></h5>
+                        </div>
+                    </div>
 
-                </div>
+                <?php } ?>
 
-            </form>
+            </div>
 
-        </div>
+            <h2>Comfy, Right?</h2>
+
+            <div class="section">
+
+                <?php foreach ($furnitures as $furniture){ ?>
+
+                    <div class="card">
+
+                        <div class="checkbox">
+                            <label for="">
+                                <input type="checkbox" class="delete-checkbox" name="delete-checkbox[]" value="<?php echo htmlspecialchars($furniture['id']); ?>">
+                                Choose to delete
+                            </label>
+                        </div>
+
+                        <div class="card_content">
+                            <h5><?php echo htmlspecialchars($furniture['sku']); ?></h5>
+                            <h3><?php echo htmlspecialchars($furniture['name']); ?></h3>
+                            <h5><?php echo htmlspecialchars($furniture['price']).'$'; ?></h5>
+                            <h5><?php echo 'Dimensions: '.htmlspecialchars($furniture['height']).'x'.htmlspecialchars($furniture['width']).'x'.htmlspecialchars($furniture['length']); ?></h5>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            </div>
+
+        </form>
+
     </div>
-
-    <?php include 'templates/footer.php'; ?>
-
 </div>
+
+<?php include 'templates/footer.php'; ?>
 
 </html>
